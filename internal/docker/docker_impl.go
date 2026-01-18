98f0797656c1eab10d42bc4978b4d8363c9ca463
santhana krishnan <29514440+hashkrish@users.noreply.github.com>
2026-01-18 22:41:48 +0530

    upgrade docker

--- internal/docker/docker_impl.go
+++ internal/docker/docker_impl.go
@@ -17,6 +17,7 @@ import (
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
+	"github.com/docker/docker/errdefs"
 	"github.com/docker/docker/pkg/stdcopy"
 	"go.containerssh.io/containerssh/config"
 	"go.containerssh.io/containerssh/internal/metrics"
@@ -147,7 +148,7 @@ loop:
 		if pullReader != nil {
 			_ = pullReader.Close()
 		}
-		if client.IsErrUnauthorized(lastError) ||
+		if errdefs.IsUnauthorized(lastError) ||
 			client.IsErrNotFound(lastError) ||
 			strings.Contains(lastError.Error(), "no basic auth credential") {
 			err = message.WrapUser(
@@ -205,7 +206,7 @@ func (d *dockerV20Client) createContainer(
 	var lastError error
 loop:
 	for {
-		var body container.ContainerCreateCreatedBody
+		var body container.CreateResponse
 		d.backendRequestsMetric.Increment()
 		body, lastError = d.dockerClient.ContainerCreate(
 			ctx,
@@ -1187,10 +1188,14 @@ loop:
 			if inspectResult.State.Status == "stopped" {
 				return nil
 			}
+			var stopTimeout = int(10)
 			lastError = d.dockerClient.ContainerStop(
 				ctx,
 				d.container.containerID,
-				&d.container.config.Timeouts.ContainerStop)
+				container.StopOptions{
+					Signal: "SIGTERM",
+					Timeout: &stopTimeout,
+				})
 			if lastError == nil {
 				return nil
 			}
@@ -1216,7 +1221,8 @@ loop:
 
 func isPermanentError(err error) bool {
 	return client.IsErrNotFound(err) ||
-		client.IsErrNotImplemented(err) ||
-		client.IsErrPluginPermissionDenied(err) ||
-		client.IsErrUnauthorized(err)
+		errdefs.IsNotImplemented(err) ||
+		// TODO: Not sure how to replace this check
+		// client.IsErrPluginPermissionDenied(err) ||
+		errdefs.IsUnauthorized(err)
 }
